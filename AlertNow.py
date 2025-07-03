8aca08be0a246559a6ec2e7531f2c805a657b5bc
Kumitokiru <161684032+Kumitokiru@users.noreply.github.com>
2025-07-03 16:21:39 +0800

    Update AlertNow.py

--- AlertNow.py
+++ AlertNow.py
@@ -642,4 +642,4 @@ if __name__ == '__main__':
         logging.error(f"Failed to initialize database: {e}", exc_info=True)
 
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=True)
+    socketio.run(app, host="0.0.0.0", port=port, debug=True, allow_unsafe_werkzeug=True)
